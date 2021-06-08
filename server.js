62a5d05b09cbf2ff1cb1476f21b00640d8f5fe04
Chelsea L <chelsealumsden@gmail.com>
2021-06-08 09:59:24 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const app = express();
+const db = mongoose.connection;
+const PORT = process.env.PORT || 3000;
+
+//-----------------------------
+// Database
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+
+//-----------------------------
+// Error messagae
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongod connected'));
+db.on('disconnected', () => console.log('mongod disconnected'));
+//-----------------------------
+// Middleware
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(methodOverride('_method'));
+
+//-----------------------------
+// Routes
+//-----------------------------
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+//-----------------------------
+// Index
+
+
+//-----------------------------
+// New
+
+
+//-----------------------------
+// Delete
+
+
+//-----------------------------
+// Update
+
+
+//-----------------------------
+// Create
+
+
+//-----------------------------
+// Edit
+
+
+//-----------------------------
+// Show
+
+
+
+
+app.listen(PORT, ()=> console.log(`Listen on port ${PORT}`));
